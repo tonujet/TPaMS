9ed7fbffb081543bd71c8111b318211191c3104b
Maksern <maks.men9922@gmail.com>
2023-05-11 10:38:06 +0300

    Division build_graphics function

--- Lab1/TaskC.py
+++ Lab1/TaskC.py
@@ -1,10 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import string
 
-
-def pdf(k, arr):
+# Helping functions
+def ProbabilityDensityFunction(k, arr):
       pdf_x = [] 
       pdf_y = []  
       n = len(arr) 
@@ -17,41 +16,54 @@ def pdf(k, arr):
                         count = count + 1
             pdf_x.append(a + i * h + h / 2)  
             pdf_y.append(count / (n * h)) 
+      pdf_x = list(map(lambda el: round(el, 2),  pdf_x))
       d = {'x': pdf_x, 'y': pdf_y}
       return d
 
-def build_graphics(number_of_intervals, arr):
-      coordinates = pdf(number_of_intervals, arr)
-      coordinates["x"] = list(map(lambda el: round(el, 2), coordinates["x"]))
-      #Діаграма розмаху
-      plt.subplot(2, 2, 1)
-      plt.title("Діаграма розмаху")
-      plt.boxplot(arr)
-
-      #Графік розподілу
-      plt.subplot(2, 2, 2)
-      plt.title("Графік розподілу")
-      plt.plot(coordinates["x"], coordinates["y"])      
-
-      #Діаграма Парето
-      df = pd.DataFrame({'numbers': coordinates["y"]})
-      df.index = coordinates["x"]
+def sortValues(x, y):
+      df = pd.DataFrame({'numbers': y})
+      df.index = x
       df = df.sort_values(by='numbers', ascending=False)
       pareto_x = []
       for i in range(0, len(df.index.values)):
             x = string.ascii_uppercase[i] + f' {df.index.values[i]}'
             pareto_x.append(x)
+      pareto = {'x': pareto_x, 'y': df["numbers"].values}
+      return pareto
 
+
+# Function to build graphics
+def buildboxPlot(arr):
+      plt.subplot(2, 2, 1)
+      plt.title("Діаграма розмаху")
+      plt.boxplot(arr)
+
+def buildPDF(x, y):
+      plt.subplot(2, 2, 2)
+      plt.title("Графік розподілу")
+      plt.plot(x, y) 
+
+def buildPareto(x, y):
+      pareto = sortValues(x, y)
       plt.subplot(2, 2, 3)
       plt.title("Діаграма Парето")
-      plt.bar(pareto_x, df["numbers"].values)
+      plt.bar(pareto["x"], pareto["y"])
 
-
-      #Кругова діаграма розподілу
+def buildPieDensity(x, y):
       explode = (0.4, 0.3, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4, 0.4)
       plt.subplot(2, 2, 4)
       plt.title("Кругова діаграма розподілу")
-      plt.pie(coordinates["y"], explode=explode, labels = coordinates["x"], autopct='%1.1f%%')
+      plt.pie(y, explode=explode, labels = x, autopct='%1.1f%%')
+
+     
+
+def build_graphics(number_of_intervals, arr):
+      coordinates = ProbabilityDensityFunction(number_of_intervals, arr)
+
+      buildboxPlot(arr)     
+      buildPDF(coordinates["x"], coordinates["y"])
+      buildPareto(coordinates["x"], coordinates["y"])
+      buildPieDensity(coordinates["x"], coordinates["y"])
 
       plt.show()
 
