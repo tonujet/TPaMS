6b87ac063afa1ce3e9ea901c2c187f080b20123f
Antonio <tonujet@gmail.com>
2023-06-08 02:33:01 +0200

    task1 refactoring

--- Lab2/Task1.py
+++ Lab2/Task1.py
@@ -1,20 +1,37 @@
+import math
 import numpy as np
-import scipy.stats as stats
-import statistics as st
+import scipy.stats as st
+import Lab1.TaskB as stats
 
-def getMeanInterval(data):
-    mean_interval = stats.norm.interval(confidence=0.95, loc = np.mean(data), scale = stats.sem(data))
+
+def mean_interval(sample, confidence):
+    n = len(sample - 1)
+    alpha = 1 - confidence
+    z = abs(st.t.ppf(alpha / 2, n - 1))
+    mean = stats.sample_mean(sample)
+    standard_deviation = stats.sample_mean_squared_deviation(sample)
+    lower_bound = mean - z * standard_deviation / math.sqrt(n)
+    upper_bound = mean + z * standard_deviation / math.sqrt(n)
+    return lower_bound, upper_bound
+
+
+def library_mean_interval(sample, confidence):
+    mean_interval = st.norm.interval(confidence=confidence, loc=stats.sample_mean(sample), scale=st.sem(sample))
     return mean_interval
 
-def getSquareDeviationInterval(data):
-    df = len(data) - 1 
-    confidence = 0.95  
+
+def square_deviation_interval(sample, confidence):
+    df = len(sample) - 1
     alpha = 1 - confidence
+    standard_deviation = stats.sample_mean_squared_deviation(sample)
 
-    chi2_lower = stats.chi2.ppf(alpha / 2, df)
-    chi2_upper = stats.chi2.ppf(1 - alpha / 2, df)
+    chi2_lower = st.chi2.ppf(alpha / 2, df)
+    chi2_upper = st.chi2.ppf(1 - alpha / 2, df)
 
-    ci_lower = np.sqrt(df * st.stdev(data)**2 / chi2_upper)
-    ci_upper = np.sqrt(df * st.stdev(data)**2 / chi2_lower)
+    lower_bound = np.sqrt(df * standard_deviation ** 2 / chi2_upper)
+    upper_bound = np.sqrt(df * standard_deviation ** 2 / chi2_lower)
 
-    return f"({ci_lower}, {ci_upper})"
+    return lower_bound, upper_bound
+
+# def library_square_deviation_interval(sample, confidence):
+    # return st.t.interval(confidence, len(sample)-1, loc=np.mean(sample), scale=st.sem(sample))
--- Lab2/main.py
+++ Lab2/main.py
@@ -1,26 +1,34 @@
 from Task1 import *
 from Task2 import *
+import Lab1.TaskB as stats
 
 n = 121
 mean_square_deviation = 1.3
 sample = np.random.normal(0.0, mean_square_deviation, n).round(decimals=2)
+confidence = 0.95
 # Variant
 print("Variant: ", 120 % 11 + 11 * 5)
 
 
 
 # Task A
-print("------------Task 1------------")
-print("Вибіркове середнє: ", st.mean(sample))
-print("Вибіркове середньоквадратичне відхилення: ", round(st.stdev(sample), 3))
+print("\n\n\t\t\t\t------------Task 1------------\n")
+print("Вибірка: ", sample)
+print("\nВибіркове середнє: ", stats.sample_mean(sample))
+print("Вибіркове середньоквадратичне відхилення: ",
+      round(stats.sample_mean_squared_deviation(sample), 3))
 
-mean_interval = getMeanInterval(sample)
-print("Проміжок для математичного сподівання з рівнем довіри 95%", mean_interval)
+print("\nДвохсторонній довірчий інтервал для математичного сподівання з рівнем довіри 95%",
+      mean_interval(sample, confidence))
+print("Результати бібліотеки\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
+      library_mean_interval(sample, confidence))
 
-mean_square_deviation_interval = getSquareDeviationInterval(sample)
-print("Проміжок для середньоквадратичного відхилення з рівнем довіри 95%", mean_square_deviation_interval)
+print("\nДвохсторонній довірчий інтервал для середньоквадратичного відхилення з рівнем довіри 95%",
+      square_deviation_interval(sample, confidence))
+# print("Результати бібліотеки\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
+#       library_square_deviation_interval(sample, confidence))
 
 # Task B
-print("------------Task 2------------")
+print("\n\n\t\t\t\t------------Task 2------------\n")
 
 
